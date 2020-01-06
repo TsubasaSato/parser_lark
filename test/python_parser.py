dce8a726d511042f12fa45b45b7ae0cd5a4e827b
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-06 22:30:36 +0900

    Update python_parser.py

--- test/python_parser.py
+++ test/python_parser.py
@@ -97,7 +97,7 @@ if __name__ == '__main__':
     print("----------tree.childrenn-----\n",tree.children)
     print("----------tree.pretty()------\n",tree.pretty())
     
-    # Tree_2(tree.find_data_topdown("decorated")
+    print(Tree_2(tree.find_data_topdown("decorated").pretty())
     
     
     
