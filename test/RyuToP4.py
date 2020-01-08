6fba7bb1272f714a102965735123e5be7ddfe29c
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-08 21:05:44 +0900

    Update RyuToP4.py

--- test/RyuToP4.py
+++ test/RyuToP4.py
@@ -23,7 +23,7 @@ class RyuToP4Transformer(Transformer):
     #変数宣言
     def expr_stmt(self,args):
         print(type(args[0]))
-        print(args[0].data,args[0].children.type)
-
+        print(args[0].data,args[0].children)
+        print(args[0].children[0])
         #print(RyuToP4Transformer(visit_tokens=True).transform(Tree("expr_stmt",args)))
 
