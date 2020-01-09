0b92984af4a5f801581b1987117a475f7eee1260
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-09 10:53:36 +0900

    Update RyuToP4.py

--- test/RyuToP4.py
+++ test/RyuToP4.py
@@ -39,7 +39,9 @@ class RyuToP4Transformer(Transformer):
         print(args)
         #Tokenの名前取得方法
         print("len:",len(args))
-
+        print(args[0].children)
+        print(args[0].children[0])
+        """
         if args[0].data=="var":
             #辞書のキーに登録
             if args[1].data=="var":
@@ -53,5 +55,6 @@ class RyuToP4Transformer(Transformer):
             else:
                 pass
         print("-----Finished-----")
+        """
         #print(RyuToP4Transformer(visit_tokens=True).transform(Tree("expr_stmt",args)))
 
