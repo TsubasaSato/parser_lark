5ff678a8dbf1c8bf086128cda580f5a203b6fd8f
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-09 17:46:48 +0900

    Update python_parser.py

--- test/python_parser.py
+++ test/python_parser.py
@@ -106,7 +106,7 @@ if __name__ == '__main__':
     #Packet_inハンドラ
     T.transform(list(data[1].find_data("funcdef"))[0])
     #RyuToP4Transformer().transform(tree)
-    print(T.get_dict("match")[2][0])
+    print(T.get_dict("match"))
     for x in T.get_dict("match")[2][0].keys():
         print(x)
     for x in T.get_dict("match")[2][0].values():
