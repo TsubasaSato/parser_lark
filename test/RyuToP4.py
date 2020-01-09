be3e1033b88193f773f5e936f626b6310369cbf8
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-09 18:44:11 +0900

    Update RyuToP4.py

--- test/RyuToP4.py
+++ test/RyuToP4.py
@@ -81,7 +81,7 @@ class RyuToP4Transformer(Transformer):
             print(get_origin_name(self.env,self.env[args[0].children[0].children[0]]))
             print("-----Original Name---match_t1--")
             print(Tree("",args[1].children).pretty())
-            print(get_origin_name(self.env,self.env[args[1].children[1].children[3].children[1]))
+            print(get_origin_name(self.env,self.env[args[1].children[1].children[3].children[1]]))
             print("-----Finished-----")
         else:
             return Tree("funccall",args)
