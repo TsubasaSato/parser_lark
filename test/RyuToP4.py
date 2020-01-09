ac89a07e42d722dacdc167fec7104a6c58375c63
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-09 10:44:56 +0900

    Update RyuToP4.py

--- test/RyuToP4.py
+++ test/RyuToP4.py
@@ -23,7 +23,7 @@ def getattr_get_var(tree):
     if len(tree.children)>1 and tree.children[0].data=="getattr":
         #getattrが続く場合
         data=getattr_get_var(tree.children[0])
-        data.append(tree.children[1].data)
+        data.append(tree.children[1])
         return data
     else:
         data=[]
