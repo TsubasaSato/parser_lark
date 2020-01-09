7b1a150e8ddbf3ff0167a827a0d0af178a865044
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-09 17:34:31 +0900

    Update RyuToP4.py

--- test/RyuToP4.py
+++ test/RyuToP4.py
@@ -1,16 +1,4 @@
 from lark import Tree, Transformer
-"""
-class Environment():
-
-    def __init__(self):
-        self.var=dict()
-        
-    def set(self,key,value):
-        self.var[key]=value
-
-    def get(self,key):
-        return self.var[key]
-"""
 
 def getattr_get_list(tree):
     """
