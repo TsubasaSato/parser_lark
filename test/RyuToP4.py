f41382c21981a89b1aa63fd4641ccd1ac16cf75e
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-09 15:46:38 +0900

    Update RyuToP4.py

--- test/RyuToP4.py
+++ test/RyuToP4.py
@@ -76,7 +76,6 @@ class RyuToP4Transformer(Transformer):
         if args[0].children[1] =="send_msg":
             print(args[1])
             print(Tree("Funccall",args).pretty())
-            print(arg_get_dict_list(args[1]))
         print("-----Finished-----")
         
     def dict_print(self):
