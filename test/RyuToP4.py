82989d746316860f7d10f5b11532b1dc8c56f444
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-09 16:14:07 +0900

    Update RyuToP4.py

--- test/RyuToP4.py
+++ test/RyuToP4.py
@@ -78,7 +78,7 @@ class RyuToP4Transformer(Transformer):
             print(Tree("Funccall",args).pretty())
             print("-----Finished-----")
         else:
-            return Tree("funccall",args[0])
+            return Tree("funccall",args)
         
         
     def dict_print(self):
