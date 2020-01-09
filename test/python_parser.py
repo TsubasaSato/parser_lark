d7afb38c314e79c4437228a7057a563aab2ab31d
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-09 13:31:26 +0900

    Update python_parser.py

--- test/python_parser.py
+++ test/python_parser.py
@@ -101,7 +101,7 @@ if __name__ == '__main__':
     data=list(tree.find_data("decorated"))
     #Switch_featuredハンドラ
     RyuToP4Transformer().transform(list(data[0].find_data("funcdef"))[0])
-    print("----------{}-----------".format(len(data)))
+    print("---------------{}----------------".format(len(data)))
     #Packet_inハンドラ
     RyuToP4Transformer().transform(list(data[1].find_data("funcdef"))[0])
     #RyuToP4Transformer().transform(tree)
