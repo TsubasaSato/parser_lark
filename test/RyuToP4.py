22931351fe2ce64f4d52caf9fd9256e204c970a5
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-10 13:40:32 +0900

    Update RyuToP4.py

--- test/RyuToP4.py
+++ test/RyuToP4.py
@@ -111,7 +111,9 @@ def arg_get_dict_list(tree):
             arg_list=funccall_get_list(x)
         elif x.data=="subscript":
             item = x.children[0].children[0]
-            item.strip("'")
+            print(item)
+            item.strip('"')
+            print(item)
             arg_list.append(item)
         else:
             arg_list.append(x.children[0])
