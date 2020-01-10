5d5e1e064e440958a59c23a90524d2bf1af42c38
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-10 11:17:37 +0900

    Update RyuToP4.py

--- test/RyuToP4.py
+++ test/RyuToP4.py
@@ -102,7 +102,7 @@ class RyuToP4Transformer(Transformer):
                 self.env[args[0].children[0]]=funccall_get_list(args[1])
             elif args[1].data=="list":
                 self.env[args[0].children[0]]=funccall_get_list(args[1].children[0])
-            elif atgs[1].data=="getitem":
+            elif args[1].data=="getitem":
                 self.env[args[0].children[0]]=funccall_get_list(args[1])
             else:
                 pass
