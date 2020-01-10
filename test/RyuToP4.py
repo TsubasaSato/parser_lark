186e95b76a323c876c2012561b46f66420195811
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-10 11:43:00 +0900

    Update RyuToP4.py

--- test/RyuToP4.py
+++ test/RyuToP4.py
@@ -109,7 +109,7 @@ class RyuToP4Transformer(Transformer):
             else:
                 pass
         if "port" in self.env:
-            print(type(get_origin_name(self.env,self.env["port"])[-1]["port"]))
+            print(get_origin_name(self.env,self.env["port"])[-1])
         print("-----Finished in expr_stmt---")
     
     def funccall(self,args):
