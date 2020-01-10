6f2fdae37f842b2a9d7e07581ed1b94977688d2d
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-10 13:19:07 +0900

    Update RyuToP4.py

--- test/RyuToP4.py
+++ test/RyuToP4.py
@@ -58,7 +58,7 @@ def get_p4src_alist(_vars,name):
                             var=get_origin_name(_vars,[data[x].value])[-1]
                         print(RyuToP4_key)
                         print(var in RyuToP4_key)
-                        print(var)
+                        print(type(var))
                         if var in RyuToP4_key:
                             p4src.append("{} = {}".format(RyuToP4_key[x],RyuToP4_key[var]))
                         else:
