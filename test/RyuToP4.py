a18ba9e4cfa2890fbb95e1532edee71c5161b408
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-10 13:16:26 +0900

    Update RyuToP4.py

--- test/RyuToP4.py
+++ test/RyuToP4.py
@@ -55,7 +55,7 @@ def get_p4src_alist(_vars,name):
                         #数字も文字列扱いされている可能性あり、要デバッグ
                         var=data[x]
                         if type(data[x].value)==type(str()):
-                            var=get_origin_name(_vars,[data[x].value])[-1]
+                            var=get_origin_name(_vars,[data[x].value])[-1].value
                         print(var in RyuToP4_key)
                         print(var)
                         if var in RyuToP4_key:
