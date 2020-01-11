6636d7d90df3c4128e7954d5cad17220b8d0a5ad
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-11 18:38:56 +0900

    Update RyuToP4.py

--- test/RyuToP4.py
+++ test/RyuToP4.py
@@ -282,15 +282,15 @@ class RyuToP4Transformer(Transformer):
             return Tree("funccall",args)
     def if_stmt(self,args):
         print("-----Start in if_stmt---")
-        print("@args@",args[0])
-        print("args[0].children[0]",args[0].children[0])
-        print(args[0].data)
+        print("@args@",args)
+        print("@args[0].children[0]@",args[0].children[0])
+        """
         if args[0].data=="funccall":
             print("funccall_get_list:",funccall_get_list(args[0]))
             print(get_p4src_packet(self.env,funccall_get_list(args[0])))
         elif args[0].children[0].data=="var":
             print(get_p4src_packet(self.env,[args[0].children[0].children[0]]))
-        
+        """
         print("-----Finished in if_stmt---")
     def get_alldicts(self):
         return self.env
