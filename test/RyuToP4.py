b1f2b276094723c1180d1ddf53e586fc83afb208
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-11 16:36:51 +0900

    Update RyuToP4.py

--- test/RyuToP4.py
+++ test/RyuToP4.py
@@ -283,7 +283,7 @@ class RyuToP4Transformer(Transformer):
     def if_stmt(self,args):
         print("-----Start in if_stmt---")
         print(args[0].children[0])
-        print(get_p4src_packet(self.env,[args[0].children[0]]))
+        print(get_p4src_packet(self.env,[args[0].children[0].children[0]]))
         print("-----Finished in expr_stmt---")
     def get_alldicts(self):
         return self.env
