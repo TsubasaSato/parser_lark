22727cf514e33dfe7101c4b36cc9a4c118125041
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-12 16:51:36 +0900

    Update RyuToP4.py

--- test/RyuToP4.py
+++ test/RyuToP4.py
@@ -56,7 +56,7 @@ class Message():
             for y in self.entries[x]:
                 if len(y)==4:
                     #pktin内で生成されたエントリ
-                    self.p4src.append(self.src_h.format(y[3],y[1],y[2]))
+                    self.p4src.append(self.src_h.format(y[3],y[1],y[2][0][0]))
                 elif y[1]:
                     #matchが空ならif文を作成しない
                     if count==1:
