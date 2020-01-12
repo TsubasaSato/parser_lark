6d2ee4c8c4ea1215414be3ea4ae508f0f53b7f32
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-12 16:19:16 +0900

    Update RyuToP4.py

--- test/RyuToP4.py
+++ test/RyuToP4.py
@@ -24,8 +24,8 @@ class Message():
     src_inst="    {inst}\n"
     src_1="if ({match}) {{\n    {inst}\n    }}\n"
     src_2="else if ({match}) {{\n    {inst}\n    }}\n"
-    src_h="bit<1> OK_{0}_1;\nbit<32> index_{0}_1;\nhash(index_{0}_1,HashAlgorithm.crc16,32w0,{{{1}}},32w65536);\n
-                reg{0}.read(OK_{0}_1,index_{0}_1);\nif (OK_{0}_1==1){{\n    {2}\n    }}\n"
+    src_h="""bit<1> OK_{0}_1;\nbit<32> index_{0}_1;\nhash(index_{0}_1,HashAlgorithm.crc16,32w0,{{{1}}},32w65536);\n
+                reg{0}.read(OK_{0}_1,index_{0}_1);\nif (OK_{0}_1==1){{\n    {2}\n    }}\n"""
     src_hh="bit<1> OK_{0}_0;\nbit<32> index_{0}_0;\nhash(index_{0}_0,HashAlgorithm.crc16,32w0,{{{1}}},32w65536);\nreg{0}.write(index_{0}_0,1w1);\n"
     p4src=[]
     p4src_pktin=[]
