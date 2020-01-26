524d2e29861ff5c0a087aedaccad0ceb7c191ab8
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-26 16:07:57 +0900

    Update test.py

--- test/test.py
+++ test/test.py
@@ -6,7 +6,6 @@ for _ in range(100):
     subprocess.call(["python3","./python_parser.py",
                            "/home/tsubasa/learnP4/TcpSyn_comment.py"])
     t2=time.time()
-    time.sleep(5)
     print(t2-t1)
     data.append(t2-t1)
 print("min/avg/max:{}/{}/{}".format(min(data),sum(data)/len(data),max(data)))
