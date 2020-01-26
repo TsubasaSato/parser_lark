dc4d199c5165bcb2122909ee1a1e43a599c34676
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-26 16:09:18 +0900

    Update python_parser.py

--- test/python_parser.py
+++ test/python_parser.py
@@ -44,20 +44,17 @@ def run_and_capture(cmd):
     # ここでプロセスが (非同期に) 開始する.
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     buf = []
-    a=[b'loading app TcpSyn_comment.py\n', b'loading app ryu.controller.ofp_handler\n', b'instantiating app TcpSyn_comment.py of TCPSYN13\n', b'instantiating app ryu.controller.ofp_handler of OFPHandler\n', b'BRICK TCPSYN13\n', b'  CONSUMES EventOFPPacketIn\n', b'  CONSUMES EventOFPSwitchFeatures\n', b'BRICK ofp_event\n', b"  PROVIDES EventOFPPacketIn TO {'TCPSYN13': {'main'}}\n", b"  PROVIDES EventOFPSwitchFeatures TO {'TCPSYN13': {'config'}}\n", b'  CONSUMES EventOFPEchoReply\n', b'  CONSUMES EventOFPEchoRequest\n', b'  CONSUMES EventOFPErrorMsg\n', b'  CONSUMES EventOFPHello\n', b'  CONSUMES EventOFPPortDescStatsReply\n', b'  CONSUMES EventOFPPortStatus\n', b'  CONSUMES EventOFPSwitchFeatures\n']
-
+    a=[b'Calling target program-options parser\n', b'Adding interface enp1s0f0 as port 1\n', b'Adding interface enp1s0f1 as port 2\n']
 
     while True:
         # バッファから1行読み込む.
         line = proc.stdout.readline()
         buf.append(line)
-        print(buf)
 
         # バッファが空 + プロセス終了.
         if buf==a:
             break
     proc.terminate()
-    print("terminate()")
     
 if __name__=='__main__':
     
