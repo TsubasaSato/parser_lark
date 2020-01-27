36bf82db1e3cab8f69bea8b839105047a19fc099
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-28 05:09:44 +0900

    Update test.py

--- test/test.py
+++ test/test.py
@@ -6,8 +6,30 @@ pps=100
 us=int(1000000/pps)
 count=pps*5
 
+def run_and_capture(cmd):
+    '''
+    :param cmd: str 実行するコマンド.
+    :rtype: str
+    :return: 標準出力.
+    '''
+    # ここでプロセスが (非同期に) 開始する.
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    buf = []
+    a=[b'Calling target program-options parser\n', b'Adding interface enp1s0f0 as port 1\n', b'Adding interface enp1s0f1 as port 2\n']
+
+    while True:
+        # バッファから1行読み込む.
+        line = proc.stdout.readline()
+        buf.append(line)
+        print(buf)
+
+        # バッファが空 + プロセス終了.
+        if buf==a:
+            break
+    proc.terminate()
+
 for _ in range(1):
-    out=subprocess.check_output(command.format(count,us),shell=True,stdout=subprocess.PIPE)
-    print(out.stdout)
+    run_and_capture(command.format(count,us))
+    
 
 
